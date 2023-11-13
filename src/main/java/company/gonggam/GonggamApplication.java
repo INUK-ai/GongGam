63e4f07dad587f9abc4eeac64b0e7b98155a82a3
INUK-ai <arheon2469@gmail.com>
2023-11-13 14:02:44 +0900

    Feat: Spring Project 생성

---
+++ src/main/java/company/gonggam/GonggamApplication.java
@@ -0,0 +1,13 @@
+package company.gonggam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonggamApplication.class, args);
+	}
+
+}
