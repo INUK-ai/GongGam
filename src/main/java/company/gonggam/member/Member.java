831de1ebb0e896409a0b01aca0117881116b97f5
INUK-ai <arheon2469@gmail.com>
2023-11-15 22:17:56 +0900

    Feat: Member 객체 생성

---
+++ src/main/java/company/gonggam/member/Member.java
@@ -0,0 +1,25 @@
+package company.gonggam.member;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member_tb")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private LocalDateTime created_at;
+}
