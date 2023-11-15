3572f56ec855bb54568726157a6692b36104d9c5
INUK-ai <arheon2469@gmail.com>
2023-11-15 22:34:08 +0900

    Feat: Article 객체 생성

---
+++ src/main/java/company/gonggam/article/Article.java
@@ -0,0 +1,24 @@
+package company.gonggam.article;
+
+import company.gonggam.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "article_tb")
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private String title;
+    private String content;
+}
--- src/main/java/company/gonggam/member/Member.java
+++ src/main/java/company/gonggam/member/Member.java
@@ -1,11 +1,14 @@
 package company.gonggam.member;
 
+import company.gonggam.article.Article;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -16,6 +19,9 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany(mappedBy = "article")
+    private List<Article> articleList = new ArrayList<>();
 
     private String name;
     private String email;
