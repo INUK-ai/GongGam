ed8e16c2bd38980ea48277bb820a3d82d7a42f1a
INUK-ai <arheon2469@gmail.com>
2023-12-06 23:36:42 +0900

    Feat: MemberRepository 틀

--- src/main/java/company/gonggam/member/dto/MemberResponseDTO.java
+++ src/main/java/company/gonggam/member/dto/MemberResponseDTO.java
@@ -1,4 +1,9 @@
 package company.gonggam.member.dto;
 
 public class MemberResponseDTO {
+
+    // 토큰 발급
+    public record authTokenDTO() {
+
+    }
 }
---
+++ src/main/java/company/gonggam/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package company.gonggam.member.repository;
+
+import company.gonggam.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+
+}
