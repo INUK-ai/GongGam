41d6c904e4dd69bcaed9da8d2f5a4a14e0668b9b
INUK-ai <arheon2469@gmail.com>
2023-12-06 13:26:25 +0900

    Feat: AnswerService 틀

--- src/main/java/company/gonggam/answer/controller/AnswerController.java
+++ src/main/java/company/gonggam/answer/controller/AnswerController.java
@@ -1,36 +1,54 @@
 package company.gonggam.answer.controller;
 
 import company.gonggam._core.utils.ApiUtils;
+import company.gonggam.answer.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static company.gonggam.answer.dto.AnswerRequestDTO.*;
+import static company.gonggam.answer.dto.AnswerResponseDTO.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class AnswerController {
 
+    private final AnswerService answerService;
+
     /*
         답변을 한 질문리스트 조회
      */
     @GetMapping("/myQuestion")
     public ResponseEntity<?> getMyQuestionList() {
-        return ResponseEntity.ok().body(ApiUtils.success(null));
+
+        getMyQuestionListDTO responseDTO = answerService.getMyQuestionList();
+
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
     /*
-        질문을 한 답변 확인
+        답변 상세 조회
      */
     @GetMapping("/{answerId}")
-    public ResponseEntity<?> getMyAnswerList(@PathVariable("answerId") Long answerId) {
-        return ResponseEntity.ok().body(ApiUtils.success(null));
+    public ResponseEntity<?> getMyAnswer(@PathVariable("answerId") Long answerId) {
+
+        getMyAnswerDTO responseDTO = answerService.getMyAnswer(answerId);
+
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
     /*
         답변 공개 여부 수정
      */
     @PatchMapping("/{answerId}/disclosure")
-    public ResponseEntity<?> changeDisclosurePolicy(@PathVariable("answerId") Long answerId) {
+    public ResponseEntity<?> changeDisclosurePolicy(
+            @PathVariable("answerId") Long answerId,
+            changeDisclosurePolicyDTO requestDTO
+            ) {
+
+        answerService.changeDisclosurePolicy(answerId, requestDTO);
+
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 
@@ -38,15 +56,24 @@ public class AnswerController {
         답변에 대한 현재 생각 추가
      */
     @PostMapping("/{answerId}/comment")
-    public ResponseEntity<?> addNewAnswer(@PathVariable("answerId") Long answerId) {
+    public ResponseEntity<?> addComment(
+            @PathVariable("answerId") Long answerId,
+            addCommentDTO requestDTO
+            ) {
+
+        answerService.addComment(answerId, requestDTO);
+
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 
     /*
-        해당 질문에 대한 답변 조회
+        해당 질문에 대한 다른 회원의 답변 조회
      */
     @GetMapping("/{answerId}/otherAnswer")
     public ResponseEntity<?> getOtherAnswerList(@PathVariable("answerId") Long answerId) {
-        return ResponseEntity.ok().body(ApiUtils.success(null));
+
+        getOtherAnswerListDTO responseDTO = answerService.getOtherAnswerList(answerId);
+
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 }
